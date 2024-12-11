a8560ae7dd4b036fced2e1312158117596061ed5
Talha <pateltalha934@gmail.com>
2024-12-11 15:52:45 +0530

    commit

--- server/indexExpress.js
+++ server/indexExpress.js
@@ -8,6 +8,30 @@ app.get("/talhaportfolio",(req,res)=>{
     res.sendFile(__dirname + "/views/portfolio.html");
 })
 
+app.get("/message",(req,res)=>{
+    res.json({"message":"This is Talha"});
+})
+
+app.use((req,res,next)=>{
+    const secretCode=req.query.secret;
+    if(secretCode==="1234"){
+        req.isAuthorized=true;
+    }
+    else{
+        req.isAuthorized=false;
+    }
+    next();
+});
+
+app.get("/auth",(req,res)=>{
+    if(req.isAuthorized){
+        res.send("You are authorized");
+    }
+    else{
+        res.send("Unauthorized");
+    }
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
